74a780a682df770e67b77d38d1dc2ac80ae8ea55
Amanda <amngo@wharton.upenn.edu>
2026-05-28 20:26:21 +1200

    Quartz sync: May 28, 2026, 8:26 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "amanda.ngo/garden",
+    baseUrl: "garden.amanda.ngo",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
